26af0c84373a3699e9550631a38417eb22175f06
danit3 <81395601+danit3@users.noreply.github.com>
2023-07-16 23:54:01 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
+-- Para la consulta del producto con más stock:
 
+SELECT * FROM productos ORDER BY stock DESC LIMIT 1;
+
+
+-- Para la consulta del producto más vendido:
+
+SELECT p.*, SUM(v.cantidad) AS total_vendido
+FROM productos p
+JOIN ventas v ON p.id = v.producto_id
+GROUP BY p.id
+ORDER BY total_vendido DESC
+LIMIT 1;
